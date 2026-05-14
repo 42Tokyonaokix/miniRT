04dbea0b1ecdb3e6b8080b07a755bbd96a70b586
kesaito09 <ksaitou1187@gmail.com>
2026-05-14 17:25:07 +0900

    main

--- srcs/main.c
+++ srcs/main.c
@@ -3,22 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: natakaha <natakaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/13 by natakaha                  #+#    #+#             */
-/*   Updated: 2026/05/13 by natakaha                 ###   ########.fr       */
+/*   Created: 2026/05/14 16:30:00 by kesaitou          #+#    #+#             */
+/*   Updated: 2026/05/14 17:24:26 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "../../libft/includes/ft_printf.h"
+#include "../../libft/includes/libft.h"
+#include <stdbool.h>
 
-/* 
+static bool	is_valid_file_suffix(const char *file_name)
+{
+	size_t	len;
+
+	len = ft_strlen(file_name);
+	if (len < 3)
+		return (false);
+	file_name += len - 3;
+	if (ft_strcmp(file_name, ".rt"))
+		return (false);
+	return (true);
+}
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	printf("miniRT: ray tracer in progress\n");
+	if (argc != 2 || !is_valid_file_suffix((const)argv[1]))
+	{
+		ft_dprintf(2, "%s", "Error\n");
+		return (1);
+	}
+	//parser()
 	return (0);
 }
- */
+ 

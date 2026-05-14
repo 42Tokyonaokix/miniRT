0f00fb09e4110b70b6c45cff1031d26a90f34db4
kesaito09 <ksaitou1187@gmail.com>
2026-05-14 17:23:47 +0900

    libft-fix

--- libft/ft_libc/ft_strcmp.c
+++ libft/ft_libc/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kesaitou <kesaitou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 16:42:23 by kesaitou          #+#    #+#             */
-/*   Updated: 2026/05/14 17:13:41 by kesaitou         ###   ########.fr       */
+/*   Updated: 2026/05/14 17:20:46 by kesaitou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while ((*s1 && *s2) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
